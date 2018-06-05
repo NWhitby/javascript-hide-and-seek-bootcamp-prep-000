eab09a37e1342e495032132c80eb22b1f09a6594
NaKia Whitby <whitby.nakia@gmail.com>
2018-06-05 13:29:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-function deepestChild(){
+function increaseRank() {
+  
+}
+
+function deepestChild() {
   let grandNode = document.getElementById('grand-node').querySelectorAll('div')
   return grandNode[grandNode.length-1]
 
