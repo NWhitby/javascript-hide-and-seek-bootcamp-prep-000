1306ec0c368474fe4f53344eef5657ae37525099
NaKia Whitby <whitby.nakia@gmail.com>
2018-06-05 13:21:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  const grandNode = document.querySelector('#grand-node');
+  let grandNode = document.querySelector('#grand-node');
   let deepestNode = grandNode.children[0];
-   for (var i = 0; !deepestNode.children[i]; i) {
+   for (let i = 0; !deepestNode.children[i]; i) {
      deepestNode = deepestNode.children[0]
 }
   return deepestNode;
