96e2a229412dc2e4d0bcda6e4a861bf2ea7e1a4e
NaKia Whitby <whitby.nakia@gmail.com>
2018-06-05 13:28:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-function deepestChild() {
-  let grandNode = document.querySelector('#grand-node');
-  let deepestNode = grandNode.children[0];
-  for (let i = 0; !deepestNode.children[i]; i) {
-    deepestNode = deepestNode.children[0]
-  }
-  return deepestNode;
+function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length-1]
+
 }
